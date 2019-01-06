c7ff562fff728a419a21fce72968d2f5a0712651
Elise <elise.casaccia@gmail.com>
2019-01-06 10:39:31 +0100

    ajout de deux fichier

---
+++ affichage.h
@@ -0,0 +1,9 @@
+#ifndef AFFICHAGE
+#define AFFICHAGE
+	
+	
+	void map (char tab[NB_BLOCS_LARGEUR][NB_BLOCS_HAUTEUR]);
+	int affichage (char tab[NB_BLOCS_LARGEUR][NB_BLOCS_HAUTEUR], SDL_Surface ecran);
+    
+
+#endif
---
+++ main.c
@@ -0,0 +1,6 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <SDL.h>
+
+#include "constante.h"
+#include "affichage.h"
