6c8e7f9b99de3ac9cf7c3900fd6c3cee491bb763
Elise <elise.casaccia@gmail.com>
2019-01-26 14:46:55 +0100

    test

--- jouer.h
+++ jouer.h
@@ -2,6 +2,7 @@
 #define JOUER
 	
 	#include "constante.h"
+	/* Lance le jeux */
 	void jouer (SDL_Surface *ecran) ;
 	void mouvement_joueur(char tab[NB_LIGNE][NB_COLONNE], int orientation, SDL_Rect *position);
 
