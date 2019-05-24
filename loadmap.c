68a37caae835d23db6c38639cf6dc1524c3ff311
Elise <elise.casaccia@gmail.com>
2019-05-24 16:43:07 +0200

    map correction

--- loadmap.c
+++ loadmap.c
@@ -33,7 +33,7 @@ void load_map(char tab[NB_LIGNE][NB_COLONNE], int niveau)
 		fichier = fopen("map4.txt", "r");
 		break;
 		case 5:
-		fichier = fopen("map2txt", "r");
+		fichier = fopen("map2.txt", "r");
 		break;
 		
 		default:;
