b72dc875c0bfdebeecefb9da9c6d3656ca633144
Richard <richard.cloos@hotmail.fr>
2019-06-01 23:14:35 +0200

    ajout des boutons dynamiques (debut)

--- menu.c
+++ menu.c
@@ -12,9 +12,9 @@
 
 void menu(int* choix, SDL_Surface *ecran)
 {
-	int x=0,y=0;
+	int x=0,y=0,jouer=0,score=0,aide=0,fin=0;
 	
-	SDL_Surface *menu = NULL, *boutonjouer = NULL, *boutonscore = NULL, *boutonaide = NULL, *boutonfin = NULL;
+	SDL_Surface *menu = NULL, *boutonjouer = NULL, *boutonscore = NULL, *boutonaide = NULL, *boutonfin = NULL, *boutonjoueractif = NULL, *boutonscoreactif = NULL, *boutonaideactif = NULL, *boutonfinactif = NULL;
 	
 	SDL_Event event;
 	SDL_Rect positionMenu, positionBoutonJouer,positionBoutonFin, positionBoutonScore, positionBoutonAide;
@@ -27,6 +27,10 @@ void menu(int* choix, SDL_Surface *ecran)
     boutonscore = IMG_Load("boutonscore.bmp");
     boutonaide = IMG_Load("boutonaide.bmp");
     boutonfin = IMG_Load("boutonfin.bmp");
+    boutonjoueractif = IMG_Load("boutonjoueractif.bmp");
+    boutonscoreactif = IMG_Load("boutonjoueractif.bmp");
+    boutonaideactif = IMG_Load("boutonjoueractif.bmp");
+    boutonfinactif = IMG_Load("boutonjoueractif.bmp");
     
     // Position des boutons 
     /** Taille boutons: 220*75 **/
@@ -56,6 +60,79 @@ void menu(int* choix, SDL_Surface *ecran)
             case SDL_QUIT:
                 *choix = 1; //QUITTER
                 break;
+            
+            /** Changement des boutons en actif **/
+            case SDL_MOUSEMOTION:
+            x=event.motion.x;
+            y=event.motion.y;
+            
+            // JOUER
+            if(((84 < x)&&(x < 306))&&((339 < y)&&(y < 416)))
+            {
+					if (jouer==0)
+					{
+						SDL_BlitSurface(boutonjoueractif, NULL, ecran, &positionBoutonJouer);
+						SDL_Flip(ecran);
+						jouer=1;
+					}
+				
+			}else
+			{
+				SDL_BlitSurface(boutonjouer, NULL, ecran, &positionBoutonJouer);
+				SDL_Flip(ecran);
+				jouer=0;
+			}
+			
+			// SCORE
+			if(((389 < x)&&(x < 611))&&((339 < y)&&(y < 416)))
+			{
+					if (score==0)
+					{
+						SDL_BlitSurface(boutonscoreactif, NULL, ecran, &positionBoutonScore);
+						SDL_Flip(ecran);
+						score=1;
+					}
+			}else
+			{
+				SDL_BlitSurface(boutonscore, NULL, ecran, &positionBoutonScore);
+				SDL_Flip(ecran);
+				score=0;
+			}
+			
+			// AIDE
+			if(((84 < x)&&(x < 306))&&((499 < y)&&(y < 576)))
+			{
+					if (aide==0)
+					{
+						SDL_BlitSurface(boutonaideactif, NULL, ecran, &positionBoutonAide);
+						SDL_Flip(ecran);
+						aide=1;
+					}
+			}else
+			{
+				SDL_BlitSurface(boutonaide, NULL, ecran, &positionBoutonAide);
+				SDL_Flip(ecran);
+				aide=0;
+			}	
+			
+			// FIN
+			if(((389 < x)&&(x < 611))&&((499 < y)&&(y < 576)))
+			{
+					if (fin==0)
+					{
+						SDL_BlitSurface(boutonfinactif, NULL, ecran, &positionBoutonFin);
+						SDL_Flip(ecran);
+						fin=1;
+					}
+			}else
+			{
+				SDL_BlitSurface(boutonfin, NULL, ecran, &positionBoutonFin);
+				SDL_Flip(ecran);
+				fin=0;
+			}		
+			
+            break;
+            
            
             case SDL_MOUSEBUTTONUP: // Clic de la souris
 				if (event.button.button == SDL_BUTTON_LEFT)
